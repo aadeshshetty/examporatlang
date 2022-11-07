987e2e12796a5b69250f42530afa02d017fed42e
aadesh_shetty <aadeshshetty123@gmail.com>
2022-11-07 12:31:45 +0530

    config for deployment

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/ExamPortal-master'));
+app.use(express.static('./dist/examportalang'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/ExamPortal-master/'}),
+    res.sendFile('index.html', {root: 'dist/examportalang/'}),
 );
 
 // Start the app by listening on the default Heroku port
